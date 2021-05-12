a104efba38926d38015b1c329307342b83d6d1fc
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2021-05-12 18:01:16 +0200

    modello fatto

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Third-party apps
+
+    #My apps
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
